02af70680870d12eb2209d9545e0b494738024da
sahil16-12 <sahil16december@gmail.com>
2025-07-04 22:51:17 +0530

    [Add Step-1] [GREEN] Passed test for two numbers input

--- src/stringCalculator.js
+++ src/stringCalculator.js
@@ -7,5 +7,8 @@ function add(numbers) {
   if (!numbers.includes(",")) {
     return parseInt(numbers, 10);
   }
+  // If the input contains two numbers separated by a comma, sum them
+  const numArray = numbers.split(",").map(Number);
+  return numArray.reduce((sum, num) => sum + num, 0);
 }
 module.exports.add = add;
